79fc53138eb267cd34f51c52867fb049f4abd743
Nikolas Souza <nikolasfelipe2201@outlook.com>
2023-07-21 14:26:40 -0300

    update index.html e CSS e Js

--- Js/main.js
+++ Js/main.js
@@ -3,16 +3,17 @@ var exclusaoDesfeita = null;
 
 function adicionarNotaFiscal() {
   var ano = document.getElementById("ano").value;
-  var valor = document.getElementById("valor").value;
+  var valor = document.getElementById("valor").value.replace(',', '.');
   var cidade = document.getElementById("cidade").value;
   var documento = document.getElementById("documento").value;
 
-
   var notaFiscal = {
     ano: ano,
-    valor: valor,
     cidade: cidade,
+
     documento: documento,
+    valor: valor,
+
     pago: false
   };
 
@@ -22,9 +23,10 @@ function adicionarNotaFiscal() {
   salvarEstoque();
 
   document.getElementById("ano").value = "";
-  document.getElementById("valor").value = "";
   document.getElementById("cidade").value = "";
   document.getElementById("documento").value = "";
+
+  document.getElementById("valor").value = "";
 }
 
 function exibirEstoque() {
@@ -38,22 +40,45 @@ function exibirEstoque() {
 
     var row = tableBody.insertRow();
     var anoCell = row.insertCell(0);
-    var valorCell = row.insertCell(1);
-    var cidadeCell = row.insertCell(2);
-    var documentoCell = row.insertCell(3);
+    var cidadeCell = row.insertCell(1);
+    var documentoCell = row.insertCell(2);
+
+ var valorCell = row.insertCell(3);
     var actionsCell = row.insertCell(4);
 
     anoCell.innerHTML = notaFiscal.ano;
-    valorCell.innerHTML = "R$ " + notaFiscal.valor;
+    valorCell.innerHTML = "R$ " + notaFiscal.valor.replace('.', ',');
     cidadeCell.innerHTML = notaFiscal.cidade;
     documentoCell.innerHTML = notaFiscal.documento;
 
     if (notaFiscal.pago) {
       valorCell.classList.add("paid");
+
+      var cancelOkButton = document.createElement("button");
+      cancelOkButton.innerHTML = "<i class='fas fa-times'></i> Cancelar OK";
+      cancelOkButton.className = "cancel-ok-button";
+      cancelOkButton.onclick = (function(index) {
+        return function() {
+          cancelarOk(index);
+        };
+      })(i);
+
+      actionsCell.appendChild(cancelOkButton);
+    } else {
+      var okButton = document.createElement("button");
+      okButton.innerHTML = "<i class='fas fa-check'></i> OK";
+      okButton.className = "ok-button";
+      okButton.onclick = (function(index) {
+        return function() {
+          marcarComoPago(index);
+        };
+      })(i);
+
+      actionsCell.appendChild(okButton);
     }
 
     var editButton = document.createElement("button");
-    editButton.innerHTML = "Alterar";
+    editButton.innerHTML = "<i class='fas fa-edit'></i> Editar";
     editButton.className = "edit-button";
     editButton.onclick = (function(index) {
       return function() {
@@ -64,7 +89,7 @@ function exibirEstoque() {
     actionsCell.appendChild(editButton);
 
     var deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Excluir";
+    deleteButton.innerHTML = "<i class='fas fa-trash'></i> Excluir";
     deleteButton.className = "delete-button";
     deleteButton.onclick = (function(index) {
       return function() {
@@ -73,18 +98,22 @@ function exibirEstoque() {
     })(i);
 
     actionsCell.appendChild(deleteButton);
-
-    var okButton = document.createElement("button");
-    okButton.innerHTML = "OK";
-    okButton.className = "ok-button";
-    okButton.onclick = (function(index) {
-      return function() {
-        marcarComoPago(index);
-      };
-    })(i);
-
-    actionsCell.appendChild(okButton);
   }
+  verificarMostrarBotaoCancelarTodas();
+}
+
+function ordenarEstoque() {
+  estoque.sort(function(a, b) {
+    var cidadeA = a.cidade.toUpperCase();
+    var cidadeB = b.cidade.toUpperCase();
+    if (cidadeA < cidadeB) {
+      return -1;
+    }
+    if (cidadeA > cidadeB) {
+      return 1;
+    }
+    return 0;
+  });
 }
 
 function excluirNotaFiscal(index) {
@@ -113,9 +142,11 @@ function abrirModalAlterar(index) {
     var modal = document.getElementById("modalAlterarNotaFiscal");
 
     document.getElementById("modalAno").value = notaFiscal.ano;
-    document.getElementById("modalValor").value = notaFiscal.valor;
     document.getElementById("modalCidade").value = notaFiscal.cidade;
+
     document.getElementById("modalDocumento").value = notaFiscal.documento;
+    document.getElementById("modalValor").value = notaFiscal.valor.replace('.', ',');
+
 
     modal.style.display = "block";
 
@@ -133,36 +164,31 @@ function abrirModalAlterar(index) {
 
 function salvarAlteracoes(index) {
   var modalAno = document.getElementById("modalAno").value;
-  var modalValor = document.getElementById("modalValor").value;
   var modalCidade = document.getElementById("modalCidade").value;
   var modalDocumento = document.getElementById("modalDocumento").value;
+
+
+  var modalValor = document.getElementById("modalValor").value.replace(',', '.');
 
   if (modalAno && modalValor && modalCidade && modalDocumento) {
     if (index < estoque.length) {
       estoque[index].ano = modalAno;
-      estoque[index].valor = modalValor;
       estoque[index].cidade = modalCidade;
       estoque[index].documento = modalDocumento;
+
+      estoque[index].valor = modalValor;
       exibirEstoque();
       salvarEstoque();
     }
     fecharModal();
+  } else {
+    alert("Por favor, preencha todos os campos.");
   }
 }
 
 function fecharModal() {
   var modal = document.getElementById("modalAlterarNotaFiscal");
   modal.style.display = "none";
-}
-
-function mostrarBotaoDesfazer() {
-  var undoButton = document.getElementById("undoButton");
-  undoButton.style.display = "inline-block";
-}
-
-function esconderBotaoDesfazer() {
-  var undoButton = document.getElementById("undoButton");
-  undoButton.style.display = "none";
 }
 
 function marcarComoPago(index) {
@@ -173,30 +199,56 @@ function marcarComoPago(index) {
   }
 }
 
-function salvarEstoque() {
-  localStorage.setItem('estoque', JSON.stringify(estoque));
-}
-
-function carregarEstoque() {
-  var estoqueSalvo = localStorage.getItem('estoque');
-  if (estoqueSalvo) {
-    estoque = JSON.parse(estoqueSalvo);
+function cancelarOk(index) {
+  if (index < estoque.length) {
+    estoque[index].pago = false;
+    exibirEstoque();
+    salvarEstoque();
   }
 }
 
-function ordenarEstoque() {
-  estoque.sort(function(a, b) {
-    var cidadeA = a.cidade.toUpperCase();
-    var cidadeB = b.cidade.toUpperCase();
-    if (cidadeA < cidadeB) {
-      return -1;
-    }
-    if (cidadeA > cidadeB) {
-      return 1;
-    }
-    return 0;
+function mostrarBotaoDesfazer() {
+  var button = document.getElementById("cancelAllOkButton");
+  button.style.display = "inline";
+}
+
+function esconderBotaoDesfazer() {
+  var button = document.getElementById("cancelAllOkButton");
+  button.style.display = "none";
+}
+
+function verificarMostrarBotaoCancelarTodas() {
+  var temOk = estoque.some(function(notaFiscal) {
+    return notaFiscal.pago;
   });
+
+  if (temOk) {
+    mostrarBotaoDesfazer();
+  } else {
+    esconderBotaoDesfazer();
+  }
+}
+
+function cancelarTodasAsNotasOk() {
+  estoque.forEach(function(notaFiscal) {
+    notaFiscal.pago = false;
+  });
+
+  exibirEstoque();
+  salvarEstoque();
+  esconderBotaoDesfazer();
+}
+
+function salvarEstoque() {
+  localStorage.setItem("estoque", JSON.stringify(estoque));
+}
+
+function carregarEstoque() {
+  var estoqueSalvo = localStorage.getItem("estoque");
+  if (estoqueSalvo) {
+    estoque = JSON.parse(estoqueSalvo);
+  }
+  exibirEstoque();
 }
 
 carregarEstoque();
-exibirEstoque();
